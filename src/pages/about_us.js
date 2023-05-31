02e6f8318b7bc4988291cd0ed56a1a0775e4a89c
unknown <amitba371996@gmail.com>
2023-05-31 11:23:34 -0700

    advrs

--- src/pages/about_us.js
+++ src/pages/about_us.js
@@ -80,28 +80,28 @@ const About = () => {
 	// }
 	
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-			setIsVisible(scrollTop > window.innerHeight);
-		};
+	// useEffect(() => {
+	// 	const handleScroll = () => {
+	// 		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	// 		setIsVisible(scrollTop > window.innerHeight);
+	// 	};
 
-		window.addEventListener('scroll', handleScroll);
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, []);
+	// 	window.addEventListener('scroll', handleScroll);
+	// 	return () => {
+	// 		window.removeEventListener('scroll', handleScroll);
+	// 	};
+	// }, []);
 
-	const scrollToTop = () => {
-		window.scrollTo({
-			top: 0,
-			behavior: 'smooth',
-		});
-	};
+	// const scrollToTop = () => {
+	// 	window.scrollTo({
+	// 		top: 0,
+	// 		behavior: 'smooth',
+	// 	});
+	// };
 
-	if (!isVisible) {
-		return null;
-	}
+	// if (!isVisible) {
+	// 	return null;
+	// }
 
 	return (
 		<>
@@ -320,13 +320,13 @@ const About = () => {
 									)}
 								/>
 								<div className="btn">
-									<button
+									{/* <button
 										id="scroll-to-top"
 										className="top_arrow_wrap"
 										onClick={scrollToTop}
 									>
 										scr
-									</button>
+									</button> */}
 								</div>
 							</>
 						) : (
@@ -354,71 +354,7 @@ const About = () => {
 										</div>
 									</div>
 								</section>
-								<section className="section">
-									<div className="secation02 bhg">
-										<div className="jhg_tfrd">
-											<div className="container">
-												<div className="row">
-													<div className="col-md-5 colmd5 fok_ng" style={{ position: 'relative' }}>
-														<div className="kj_bgvcdfs">
-
-															<div className="image_gnbdd">
-
-																<div className="image_hfgfss">
-																	<video
-
-																		id="myVideo"
-																		preload=""
-																		src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/earth-65103.mp4"
-																		ref={videoRef}
-																		autoPlay
-																		muted
-																		onEnded={handleVideoEnded}
-
-																		className="video"
-																	>
-
-																	</video>
-																</div>
-															</div>
-														</div>
-													</div>
-													<div className="col-md-7">
-														<div className="our_about_wrap" dangerouslySetInnerHTML={{ __html: data.wpPage.about.description }}>
-
-
-
-														</div>
-													</div>
-													<div class="col-md-5 fok_ng_hggth" style={{ position: 'relative' }}>
-														<div class="kj_bgvcdfs">
-															<div class="image_gnbdd">
-																<div class="roghrtf">
-
-																</div>
-																<div className="image_hfgfss">
-																	<video
-																		id="myVideo"
-																		preload=""
-																		src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/earth-65103.mp4"
-																		ref={videoRef}
-																		autoPlay
-																		muted
-																		onEnded={handleVideoEnded}
-
-																		className="video"
-																	>
-																	</video>
-																</div>
-															</div>
-
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</section>
+								<AboutVideo/>
 								<section className="section">
 									<div className="secatioonteam bhg">
 										<div className="jhg_tfrd paddint_top padding_Btnh">
