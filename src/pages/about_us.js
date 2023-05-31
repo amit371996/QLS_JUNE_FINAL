6e5c424fbfc29f2bb594a6389fcbf32289b4b216
unknown <amitba371996@gmail.com>
2023-05-31 11:14:56 -0700

    about video

--- src/pages/about_us.js
+++ src/pages/about_us.js
@@ -7,39 +7,12 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import NavBar from "../components/Header/nav-bar";
 import Footer from "../components/footer/footer";
 import ScrollToTopButton from "../components/scroll_top/scroll_top";
+import AboutVideo from "./about_video";
 
 const About = () => {
 	const [isFullpage, setIsFullpage] = useState(true);
 	const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
 	const [isVisible, setIsVisible] = useState(false);
-
-	const videoRef = useRef(null);
-	useEffect(() => {
-	  const video = videoRef.current;
-  
-	  // Show loading animation.
-  
-	  // Play the video and handle the result using promises.
-	  const playPromise = video.play();
-  
-	  if (playPromise !== undefined) {
-		playPromise
-		  .then(() => {
-			video.play();
-		  })
-		  .catch(error => {
-			// Auto-play was prevented
-			// Show paused UI.
-		  });
-	  }
-	}, []);
-	const handleVideoEnded = () => {
-	  const video = videoRef.current;
-	  video.play();
-	};
-
-	
-	
 	useEffect(() => {
 		const handleResize = () => {
 			if (window.innerWidth < 992) {
@@ -222,46 +195,7 @@ const About = () => {
 														</div>
 													</section>
 													<section className="section">
-														<div className="secation02 bhg">
-															<div className="jhg_tfrd">
-																<div className="container">
-																	<div className="row">
-																		<div className="col-md-5 colmd5" style={{ position: 'relative' }}>
-																			<div className="kj_bgvcdfs">
-
-																				<div className="image_gnbdd">
-
-																					<div className="image_hfgfss">
-																						<video
-																							id="myVideo"
-																							preload=""
-																							src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/earth-65103.mp4"
-																							ref={videoRef}
-																							autoPlay
-																							muted
-																							onEnded={handleVideoEnded}
-
-																							className="video"
-
-																						>
-
-																						</video>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																		<div className="col-md-7">
-																			<div className="our_about_wrap" dangerouslySetInnerHTML={{ __html: data.wpPage.about.description }}>
-
-
-
-																			</div>
-
-																		</div>
-																	</div>
-																</div>
-															</div>
-														</div>
+														<AboutVideo/>
 													</section>
 													<section className="section" >
 														<div className="secatioonteam bhg">
