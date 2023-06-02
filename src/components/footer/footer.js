ca89b3ad591c6633c78a3126320e2dad56daa289
amit371996 <amitba371996@gmail.com>
2023-06-02 01:07:10 -0700

    ssddd

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -54,7 +54,7 @@ export default function Footer() {
 												<iframe
 													id="myIframe"
 													src="https://www.qlspace.com.au/newsletter/"
-													// scrolling="no" style={{ minHeight: '470px !important' }}
+													 scrolling="no" style={{ width:'100%',  }}
 												></iframe>
 												{/* <form className="row g-3">
 													<div className="col-md-6">
