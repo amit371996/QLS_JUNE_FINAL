d0b342215925afc343d6119e58010a68f17896a1
amit371996 <amitba371996@gmail.com>
2023-06-05 04:32:46 -0700

    css changs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,7 @@ import { Link, StaticQuery, graphql } from "gatsby"
 import Layout from '../components/layout/layout';
 import { useState } from 'react';
 export default function Blog() {
+    const [showFullText, setShowFullText] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 6;
     return (
@@ -49,67 +50,67 @@ export default function Blog() {
                 const startIndex = (currentPage - 1) * itemsPerPage;
                 const endIndex = startIndex + itemsPerPage;
                 const handlePageClick = (pageNumber) => {
-					setCurrentPage(pageNumber);
-				};
+                    setCurrentPage(pageNumber);
+                };
 
-				const renderPageNumbers = () => {
-					const pageNumbers = [];
+                const renderPageNumbers = () => {
+                    const pageNumbers = [];
 
-					if (currentPage > 1) {
-						pageNumbers.push(
-							<li key="prev">
-								<a href="#" onClick={() => handlePageClick(currentPage - 1)}>
-									&laquo;
-								</a>
-							</li>
-						);
-					}
+                    if (currentPage > 1) {
+                        pageNumbers.push(
+                            <li key="prev">
+                                <a href="#" onClick={() => handlePageClick(currentPage - 1)}>
+                                    &laquo;
+                                </a>
+                            </li>
+                        );
+                    }
 
-					if (currentPage > 2) {
-						pageNumbers.push(
-							<li key="first">
-								<a href="#" onClick={() => handlePageClick(1)}>1</a>
-							</li>
-						);
+                    if (currentPage > 2) {
+                        pageNumbers.push(
+                            <li key="first">
+                                <a href="#" onClick={() => handlePageClick(1)}>1</a>
+                            </li>
+                        );
 
-						if (currentPage > 3) {
-							pageNumbers.push(
-								<li key="dots-1">
-									<span className="dots">...</span>
-								</li>
-							);
-						}
-					}
+                        if (currentPage > 3) {
+                            pageNumbers.push(
+                                <li key="dots-1">
+                                    <span className="dots">...</span>
+                                </li>
+                            );
+                        }
+                    }
 
-					for (let i = 1; i <= totalPages; i++) {
-						if (i === 1 || i === totalPages || (i >= currentPage - 1 && i <= currentPage + 1)) {
-							pageNumbers.push(
-								<li key={i} >
-									<a href="#" className={currentPage === i ? 'active' : null} onClick={() => handlePageClick(i)}>{i}</a>
-								</li>
-							);
-						} else if (i === currentPage - 2 || i === currentPage + 2) {
-							pageNumbers.push(
-								<li key={`dots-${i}`}>
-									<span className="dots">...</span>
-								</li>
-							);
-						}
-					}
+                    for (let i = 1; i <= totalPages; i++) {
+                        if (i === 1 || i === totalPages || (i >= currentPage - 1 && i <= currentPage + 1)) {
+                            pageNumbers.push(
+                                <li key={i} >
+                                    <a href="#" className={currentPage === i ? 'active' : null} onClick={() => handlePageClick(i)}>{i}</a>
+                                </li>
+                            );
+                        } else if (i === currentPage - 2 || i === currentPage + 2) {
+                            pageNumbers.push(
+                                <li key={`dots-${i}`}>
+                                    <span className="dots">...</span>
+                                </li>
+                            );
+                        }
+                    }
 
-					if (currentPage < totalPages) {
-						pageNumbers.push(
-							<li key="next">
-								<a href="#" onClick={() => handlePageClick(currentPage + 1)}>
-								&raquo;
-								</a>
-							</li>
-						);
-					}
-					return pageNumbers
-					
-				};
-                  
+                    if (currentPage < totalPages) {
+                        pageNumbers.push(
+                            <li key="next">
+                                <a href="#" onClick={() => handlePageClick(currentPage + 1)}>
+                                    &raquo;
+                                </a>
+                            </li>
+                        );
+                    }
+                    return pageNumbers
+
+                };
+
                 return (
                     <Layout>
                         <div>
@@ -157,44 +158,52 @@ export default function Blog() {
                                     <div className="container">
                                         <div className="row">
                                             {blogs.slice(startIndex, endIndex).map((blog) => {
-                                                return(
+                                                const headingtitle = blog.title.split('');
+                                                let displayedText;
+                                                if (showFullText) {
+                                                    displayedText = text;
+                                                } else {
+                                                    displayedText = words.slice(0, 7).join(' ');
+                                                }
+
+                                                return (
                                                     <div className="col-md-6" key={blog.id} >
-                                                    <div className="our_lkhb">
-                                                        <div className="imh_bc">
-                                                            <div className="inner_ghv">
-                                                                <a href="https://www.qlspace.com.au/from-space-to-the-ground-how-satellite-monitoring-helps-predict-and-respond-to-natural-disasters/">
+                                                        <div className="our_lkhb">
+                                                            <div className="imh_bc">
+                                                                <div className="inner_ghv">
+                                                                    <a href="https://www.qlspace.com.au/from-space-to-the-ground-how-satellite-monitoring-helps-predict-and-respond-to-natural-disasters/">
 
-                                                                    <img src={blog.news.archiveImage.sourceUrl} />
-                                                                </a>
-                                                            </div>
-                                                            <div className="date_wrad_news">
-                                                                <div className="date_rn">
-                                                                    <div className="date_icon"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/icon-_calendar-outline_.png" /></span>{blog.featuredImage.node.date}</div>
+                                                                        <img src={blog.news.archiveImage.sourceUrl} />
+                                                                    </a>
                                                                 </div>
-                                                                <div className="oir_amin">
-                                                                    <div className="uhgbv"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/userfv.png" /></span>{blog.featuredImage.node.author && blog.featuredImage.node.author.node && blog.featuredImage.node.author.node.name}</div>
+                                                                <div className="date_wrad_news">
+                                                                    <div className="date_rn">
+                                                                        <div className="date_icon"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/icon-_calendar-outline_.png" /></span>{blog.featuredImage.node.date}</div>
+                                                                    </div>
+                                                                    <div className="oir_amin">
+                                                                        <div className="uhgbv"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/userfv.png" /></span>{blog.featuredImage.node.author && blog.featuredImage.node.author.node && blog.featuredImage.node.author.node.name}</div>
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                            <div className="heading_nmb">
-                                                                <h4>
-                                                                    <Link to={"/blog/" + blog.slug}>
-                                                                        {blog.title.split('').slice(0,7).join(' ')}  </Link>
-                                                                </h4>
+                                                                <div className="heading_nmb">
+                                                                    <h4>
+                                                                        <Link to={"/blog/" + blog.slug}>
+                                                                        {headingtitle.length > 7 && !showFullText && '...'}  </Link>
+                                                                    </h4>
 
 
-                                                                <div className="next_page">
-                                                                    <Link to={"/blog/" + blog.slug} className="read-more"> Read more
-                                                                        <span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/arrowright.png" /></span>
-                                                                    </Link>
+                                                                    <div className="next_page">
+                                                                        <Link to={"/blog/" + blog.slug} className="read-more"> Read more
+                                                                            <span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/arrowright.png" /></span>
+                                                                        </Link>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
                                                 )
-                                                
+
                                             })}
-                                            
+
                                             <ul className="pagination"> {renderPageNumbers()}</ul>
 
                                         </div>
