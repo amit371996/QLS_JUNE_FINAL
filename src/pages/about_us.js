e76c57fb2d68079e3fb368b91b4caca0366ddf93
amit371996 <amitba371996@gmail.com>
2023-06-07 06:13:46 -0700

    video changes

--- src/pages/about_us.js
+++ src/pages/about_us.js
@@ -39,6 +39,17 @@ const About = () => {
 	const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
 	const handleLeave = (origin, destination) => {
 		setCurrentSectionIndex(destination.index);
+		const video = videoRef.current;
+		const playPromise = video.play();
+		if (playPromise !== undefined && origin.index === 2) {
+			playPromise
+				.then(() => {
+					video.play();
+				})
+				.catch(error => {
+
+				});
+		}
 	};
 	useEffect(() => {
 		const header = document.querySelector('header');
@@ -58,46 +69,38 @@ const About = () => {
 	}, [currentSectionIndex]);
 	const videoRef = useRef(null);
 
-	useEffect(() => {
-		handleVideoLoop();
-	}, []);
-	const handleVideoEnded = (origin, destination, direction) => {
-		const video = videoRef.current;
-		if(origin.index === 2){
-			video.play();
-		}
-		else{
-			video.play();
-		}
-		
-	};
-	const handleVideoLoop =(origin, destination, direction)=>{
-		const video = videoRef.current;
-		const playPromise = video.play();
-		if (playPromise !== undefined && origin.index === 2) {
-			playPromise
-				.then(() => {
-					video.play();
-				})
-				.catch(error => {
+	// useEffect(() => {
+	// 	const video = videoRef.current;
+	// 	const playPromise = video.play();
+	// 	if (playPromise !== undefined) {
+	// 		playPromise
+	// 			.then(() => {
+	// 				video.play();
+	// 			})
+	// 			.catch(error => {
 
-				});
-		}
-	}
+	// 			});
+	// 	}
+	// }, []);
+	const handleVideoEnded = () => {
+		const video = videoRef.current;
+		video.play();
+	};
+
 	const handleAfterLoad = (origin, destination, direction) => {
 		const scrollToTopButton = document.getElementById('scroll-to-top');
 		if (destination.index > 0) {
-		  scrollToTopButton.style.display = 'block';
+			scrollToTopButton.style.display = 'block';
 		} else {
-		  scrollToTopButton.style.display = 'none';
+			scrollToTopButton.style.display = 'none';
 		}
-	  };
-	
-	  const handleScrollToTop = () => {
+	};
+
+	const handleScrollToTop = () => {
 		fullPageState.moveTo(1);
-	  };
-	
-	  
+	};
+
+
 
 
 	return (
@@ -167,195 +170,196 @@ const About = () => {
 									render={({ fullpageApi }) => {
 										setFullPageState(fullpageApi);
 										return (
-										<>
-											<ReactFullpage.Wrapper>
-												<main>
+											<>
+												<ReactFullpage.Wrapper>
+													<main>
 
-													<section className="section">
-														<div className="secation01 bhg secn_abouts">
-															<div className="container">
-																<div className="row">
-																	<div className="col-md-12">
-																		<div className="our_main_heading text-center">
-																			<div className="bread_cumg" dangerouslySetInnerHTML={{ __html: data.wpPage.about.heading }}>
+														<section className="section">
+															<div className="secation01 bhg secn_abouts">
+																<div className="container">
+																	<div className="row">
+																		<div className="col-md-12">
+																			<div className="our_main_heading text-center">
+																				<div className="bread_cumg" dangerouslySetInnerHTML={{ __html: data.wpPage.about.heading }}>
 
+																				</div>
 																			</div>
-																		</div>
-																		<div className="img_arrow">
-																			<div className="roww_wrap">
-																				<a href="#">
-																					<img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" />
-																				</a>
+																			<div className="img_arrow">
+																				<div className="roww_wrap">
+																					<a href="#">
+																						<img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" />
+																					</a>
+																				</div>
 																			</div>
 																		</div>
 																	</div>
 																</div>
 															</div>
-														</div>
-													</section>
-													<section className="section">
-														<div className="secation02 bhg">
-															<div className="jhg_tfrd">
-																<div className="container">
-																	<div className="row">
-																		<div className="col-md-5 colmd5" style={{ position: 'relative' }}>
-																			<div className="kj_bgvcdfs">
+														</section>
+														<section className="section">
+															<div className="secation02 bhg">
+																<div className="jhg_tfrd">
+																	<div className="container">
+																		<div className="row">
+																			<div className="col-md-5 colmd5" style={{ position: 'relative' }}>
+																				<div className="kj_bgvcdfs">
 
-																				<div className="image_gnbdd">
+																					<div className="image_gnbdd">
 
-																					<div className="image_hfgfss">
-																						<video
-																							id="myVideo"
-																							preload=""
-																							src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/earth-65103.mp4"
-																							ref={videoRef}
-																							autoPlay
-																							muted
-																							onEnded={handleVideoEnded}
+																						<div className="image_hfgfss">
+																							<video
+																								id="myVideo"
+																								preload=""
+																								src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/earth-65103.mp4"
+																								ref={videoRef}
+																								autoPlay
+																								muted
+																								onEnded={handleVideoEnded}
 
-																							className="video"
+																								className="video"
 
-																						>
+																							>
 
-																						</video>
+																							</video>
+																						</div>
+																					</div>
+																				</div>
+																			</div>
+																			<div className="col-md-7">
+																				<div className="our_about_wrap" dangerouslySetInnerHTML={{ __html: data.wpPage.about.description }}>
+
+
+
+																				</div>
+
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</section>
+														<section className="section" >
+															<div className="secatioonteam bhg">
+																<div className="jhg_tfrd paddint_top padding_Btnh">
+																	<div className="container">
+																		<div className="row">
+																			<div className="col-md-12">
+																				<div className="main_heading">
+																					<div className="our_about_wrap text-start" dangerouslySetInnerHTML={{ __html: data.wpPage.about.teamheading }}>
+
 																					</div>
 																				</div>
 																			</div>
 																		</div>
-																		<div className="col-md-7">
-																			<div className="our_about_wrap" dangerouslySetInnerHTML={{ __html: data.wpPage.about.description }}>
+																		<div className="our_vba">
+																			<div className="row mar_yes">
+																				{data &&
+																					data.wpPage &&
+																					data.wpPage.about &&
+																					data.wpPage.about.ourteam.map((prop, i) => {
+																						return (
+																							<>
+																								<div className="col-xsm-12 col-sm-6 col-md-6 col-lg-3 teambx">
+																									<div className="image_tema">
+																										<div className="f1_container">
+																											<div className="f1_card shadow">
+																												<div className="front face">
 
+																													<img src={prop.image.sourceUrl} />
+																												</div>
+																												<div className="back face center">
 
-
+																													<img src={prop.colourImage.sourceUrl} />
+																												</div>
+																											</div>
+																										</div>
+																										<div className="details">
+																											<div className="left_part_name">
+																												<h4>{prop.name}</h4>
+																												<span>
+																													{prop.designation}
+																												</span>
+																											</div>
+																											<div className="right_part_name">
+																												<a href=
+																													{prop.linkedinurl}
+																													target="_blank"
+																												>
+																													<img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/linkdin.png" />
+																												</a>
+																											</div>
+																										</div>
+																									</div>
+																								</div>
+																							</>
+																						)
+																					})}
 																			</div>
-
 																		</div>
 																	</div>
 																</div>
 															</div>
-														</div>
-													</section>
-													<section className="section" >
-														<div className="secatioonteam bhg">
-															<div className="jhg_tfrd paddint_top padding_Btnh">
+														</section>
+														<section className="section">
+															<div className="section04 bhg">
 																<div className="container">
-																	<div className="row">
-																		<div className="col-md-12">
-																			<div className="main_heading">
-																				<div className="our_about_wrap text-start" dangerouslySetInnerHTML={{ __html: data.wpPage.about.teamheading }}>
+																	<div className="row align-items-center">
+																		<div className="col-md-6">
+																			<div className="ourr_tsgdf">
+																				<ul>
+																					<li>
+																						<ul>
+																							{
+																								getSection(data, 0, 5).map(url => (
+																									<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
+																								))
+																							}
+																						</ul>
+																					</li>
+																					<li>
+																						<ul>
+																							{
+																								getSection(data, 5, 10).map(url => (
+																									<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
+																								))
+																							}
+																						</ul>
+																					</li>
+																					<li>
+																						<ul>
 
+																							{
+																								getSection(data, 10, 15).map(url => (
+																									<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
+																								))
+																							}
+																						</ul>
+																					</li>
+																					<div className="clr"></div>
+																				</ul>
+																			</div>
+																		</div>
+																		<div className="col-md-6">
+																			<div className="our_about_wrap" >
+																				<div className='' dangerouslySetInnerHTML={{ __html: data.wpPage.homePartnerSection.partnerDetail }}>
 																				</div>
 																			</div>
 																		</div>
 																	</div>
-																	<div className="our_vba">
-																		<div className="row mar_yes">
-																			{data &&
-																				data.wpPage &&
-																				data.wpPage.about &&
-																				data.wpPage.about.ourteam.map((prop, i) => {
-																					return (
-																						<>
-																							<div className="col-xsm-12 col-sm-6 col-md-6 col-lg-3 teambx">
-																								<div className="image_tema">
-																									<div className="f1_container">
-																										<div className="f1_card shadow">
-																											<div className="front face">
-
-																												<img src={prop.image.sourceUrl} />
-																											</div>
-																											<div className="back face center">
-
-																												<img src={prop.colourImage.sourceUrl} />
-																											</div>
-																										</div>
-																									</div>
-																									<div className="details">
-																										<div className="left_part_name">
-																											<h4>{prop.name}</h4>
-																											<span>
-																												{prop.designation}
-																											</span>
-																										</div>
-																										<div className="right_part_name">
-																											<a href=
-																												{prop.linkedinurl}
-																												target="_blank"
-																											>
-																												<img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/linkdin.png" />
-																											</a>
-																										</div>
-																									</div>
-																								</div>
-																							</div>
-																						</>
-																					)
-																				})}
-																		</div>
-																	</div>
 																</div>
 															</div>
-														</div>
-													</section>
-													<section className="section">
-														<div className="section04 bhg">
-															<div className="container">
-																<div className="row align-items-center">
-																	<div className="col-md-6">
-																		<div className="ourr_tsgdf">
-																			<ul>
-																				<li>
-																					<ul>
-																						{
-																							getSection(data, 0, 5).map(url => (
-																								<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
-																							))
-																						}
-																					</ul>
-																				</li>
-																				<li>
-																					<ul>
-																						{
-																							getSection(data, 5, 10).map(url => (
-																								<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
-																							))
-																						}
-																					</ul>
-																				</li>
-																				<li>
-																					<ul>
 
-																						{
-																							getSection(data, 10, 15).map(url => (
-																								<li><a href={url.siteUrl}><img src={url.partnerImage.sourceUrl} alt='' /> </a> </li>
-																							))
-																						}
-																					</ul>
-																				</li>
-																				<div className="clr"></div>
-																			</ul>
-																		</div>
-																	</div>
-																	<div className="col-md-6">
-																		<div className="our_about_wrap" >
-																			<div className='' dangerouslySetInnerHTML={{ __html: data.wpPage.homePartnerSection.partnerDetail }}>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-															</div>
-														</div>
+														</section>
+														<Footer />
+													</main>
 
-													</section>
-													<Footer />
-												</main>
+												</ReactFullpage.Wrapper>
 
-											</ReactFullpage.Wrapper>
-
-										</>
+											</>
 
 
-									)}}
+										)
+									}}
 								/>
 								<div className="btn">
 									<button
@@ -363,7 +367,7 @@ const About = () => {
 										className="top_arrow_wrap"
 										onClick={() => handleScrollToTop()}
 									>
-										 <FontAwesomeIcon icon={faAngleUp} />
+										<FontAwesomeIcon icon={faAngleUp} />
 									</button>
 								</div>
 							</>
